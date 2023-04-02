3f8caf2429b882fd9a47f79f91191b55e34356a7
harsh.buddhadev@omen15 <buddhadevharsh953@gmail.com>
2023-04-02 17:52:49 +0530

    Update file install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 #	python 3
-sudo apt update && sudo apt upgrade -y || "Error Updating"
-sudo apt install python3 || "Error Installing Python3"
+sudo apt update && sudo apt upgrade -y || echo "Error Updating"
+sudo apt install python3 || echo "Error Installing Python3"
 #	pip
-sudo apt install python3-pip || "Error Installing Pip"
+sudo apt install python3-pip || echo "Error Installing Pip"
 #	Google API
-pip3 install google-auth-oauthlib || "Error installing google-auth-oauthlib"
-pip3 install google-api-python-client || "Error Installing google-api-python-client"
-pip3 install google-auth || "Error installing google-auth"
+pip3 install google-auth-oauthlib || echo "Error installing google-auth-oauthlib"
+pip3 install google-api-python-client || echo "Error Installing google-api-python-client"
+pip3 install google-auth || echo "Error installing google-auth"
 #	Fingerprint
-pip3 install adafruit-circuitpython-fingerprint || "Error Installing adafruit-circuitpython-fingerprint"
+pip3 install adafruit-circuitpython-fingerprint || echo "Error Installing adafruit-circuitpython-fingerprint"
 echo "Installion Complete"
